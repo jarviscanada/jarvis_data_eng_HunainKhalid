99d8023abd872246e143de4826819ea092a23af7
Hunain Khalid <hunainkhalid101@gmail.com>
2022-02-03 20:57:07 +0000

    worked on grep implementation

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
@@ -0,0 +1,63 @@
+package ca.jrvs.apps.grep;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public interface JavaGrep
+{
+    /**
+     * Top level search code
+     * @throws IOException
+     */
+    void process() throws IOException;
+
+    /**
+     * Traverse a given directory and return
+     * all files
+     *
+     * @param rootDir : input directory
+     * @return files under the rootDir
+     */
+    List<File> listFiles(String rootDir);
+
+    /**
+     * Read a file and return all lines
+     *
+     * @param inputfile : file to be read
+     * @return lines
+     * @throws  IllegalArgumentException if the
+     *          given inputFile is not a file.
+     */
+    List<String> readLines(File inputfile);
+
+    /**
+     * Check if a line contains the regex
+     * pattern passed by the user
+     *
+     * @param line: input string
+     * @return true if there is a match, false
+     *          otherwise
+     */
+    boolean containsPattern(String line);
+
+    /**
+     * Write lines to file
+     *
+     * @param lines : matched line
+     * @throws IOException if write fails
+     */
+    void writetoFile(List<String> lines) throws IOException;
+
+    String getRootPath();
+
+    void setRootPath(String rootPath);
+
+    String getRegex();
+
+    void setRegex(String regex);
+
+    String getOutFile();
+
+    void setOutFile(String outfile);
+}
---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,97 @@
+package ca.jrvs.apps.grep;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JavaGrepImp implements JavaGrep
+{
+    final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
+
+    private String regex;
+    private String rootPath;
+    private String outFile;
+
+    public static void main(String[] args)
+    {
+        if (args.length != 3)
+        {
+            throw new IllegalArgumentException("USAGE: JavaGrep [regex] [rootPath] [outFile]");
+        }
+
+        JavaGrepImp javaGrepImp = new JavaGrepImp();
+        javaGrepImp.setRegex(args[0]);
+        javaGrepImp.setRootPath(args[1]);
+        javaGrepImp.setOutFile(args[2]);
+
+        try
+        {
+            javaGrepImp.process();
+        }
+        catch (Exception ex)
+        {
+            javaGrepImp.logger.error("Error: Unable to process", ex);
+        }
+
+    }
+
+    @Override
+    public void process() throws IOException
+    {
+
+    }
+
+    @Override
+    public List<File> listFiles(String rootDir) {
+        return null;
+    }
+
+    @Override
+    public List<String> readLines(File inputfile) {
+        return null;
+    }
+
+    @Override
+    public boolean containsPattern(String line) {
+        return false;
+    }
+
+    @Override
+    public void writetoFile(List<String> lines) throws IOException {
+
+    }
+
+    @Override
+    public void setRegex(String regex)
+    {
+        this.regex = regex;
+    }
+
+    @Override
+    public String getRegex()
+    {
+        return this.regex;
+    }
+
+    @Override
+    public String getRootPath() {
+        return rootPath;
+    }
+
+    @Override
+    public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    @Override
+    public String getOutFile() {
+        return this.outFile;
+    }
+
+    @Override
+    public void setOutFile(String outfile) {
+        this.outFile = outfile;
+    }
+}
