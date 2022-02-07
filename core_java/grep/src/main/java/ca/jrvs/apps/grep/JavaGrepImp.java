2a63d9d19c71e4546fa2fae7e3f6836280744436
Hunain Khalid <hunainkhalid101@gmail.com>
2022-02-07 20:29:24 +0000

    updated grep first impl. code

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -3,56 +3,89 @@
  * Purpose: Simulate "grep" command in java
  * Filename: javaGrepImp.java
  */
-
 package ca.jrvs.apps.grep;
 
 import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
-// Main method to take 3 arguments, processed via helper methods
 public class JavaGrepImp implements JavaGrep
 {
     final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
-
     private String regex;
     private String rootPath;
     private String outFile;
 
+    // Main method to take 3 arguments, processed via helper methods
     public static void main(String[] args)
     {
+        //Fail-Fast, Fail safe
         if (args.length != 3)
         {
             throw new IllegalArgumentException("USAGE: JavaGrep [regex] [rootPath] [outFile]");
         }
 
+        //Attain and set args
         JavaGrepImp javaGrepImp = new JavaGrepImp();
         javaGrepImp.setRegex(args[0]);
         javaGrepImp.setRootPath(args[1]);
         javaGrepImp.setOutFile(args[2]);
 
+        //Attempt processing of data, handle any errors
         try
         {
             javaGrepImp.process();
         }
         catch (Exception ex)
         {
-            javaGrepImp.logger.error("Error: Unable to process", ex);
+            javaGrepImp.logger.error("Error: Unable to process: ", ex);
         }
-
     }
 
+    //Create new list to store lines that are matched
     @Override
     public void process() throws IOException
     {
+        List<String> linesMatched = new ArrayList<String>();
 
+        try
+        {
+            for (File f : listFiles())
+        }
+        catch ()
+        {}
     }
 
+    // Used built in Java 8 API to recursively list
+    // files.
     @Override
-    public List<File> listFiles(String rootDir) {
-        return null;
+    public List<File> listFiles(String rootDir)
+    {
+        List<File> fileList = new ArrayList<File>();
+
+        /* Find files in the root directory, and add
+        to fileList. only if directories have files.
+        Else, log error as "unable to list files". */
+        try
+        {
+            Files.walk(Paths.get(rootDir)).forEach(dir ->
+            {
+                if (dir.toFile().isFile())
+                {
+                    fileList.add(dir.toFile());
+                }
+            });
+        }
+        catch (IOException e)
+        {
+            logger.error("Error: Unable to list files: ", e);
+            return null;
+        }
+        return fileList;
     }
 
     @Override
@@ -83,22 +116,26 @@ public class JavaGrepImp implements JavaGrep
     }
 
     @Override
-    public String getRootPath() {
+    public String getRootPath()
+    {
         return rootPath;
     }
 
     @Override
-    public void setRootPath(String rootPath) {
+    public void setRootPath(String rootPath)
+    {
         this.rootPath = rootPath;
     }
 
     @Override
-    public String getOutFile() {
+    public String getOutFile()
+    {
         return this.outFile;
     }
 
     @Override
-    public void setOutFile(String outfile) {
+    public void setOutFile(String outfile)
+    {
         this.outFile = outfile;
     }
 }
