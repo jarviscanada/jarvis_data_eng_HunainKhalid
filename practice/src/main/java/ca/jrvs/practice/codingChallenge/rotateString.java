771f89ec76244fec2277d56e249579bd172a85f0
Hunain Khalid <hunainkhalid101@gmail.com>
2022-03-23 20:43:27 +0000

    understood one-pass sol to rotateString

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/rotateString.java
@@ -0,0 +1,25 @@
+package ca.jrvs.practice.codingChallenge;
+
+public class rotateString
+{
+  public boolean rotateString(String s, String goal)
+  {
+    /*
+      At first was going to create new string permutations of
+      all strings so O(n^2) possibilities in the
+      search space,
+      then another for-loop iterating and searching for a match
+      between String s and String goal.
+
+      This would have taken O(n^2) + O(n), O(n^2) time.
+      The single pass solution appears below.
+
+      Adding goal onto itself, yields xyzxyz, going through the
+      list searching for s, which has all permutations
+      and s goes through "goalgoal
+     */
+
+   return s.length() == goal.length() && (goal + goal).contains(s);
+  }
+
+}
