80576aed3a9c129e7e4c860d06c4172db59105f1
Hunain Khalid <hunainkhalid101@gmail.com>
2022-03-24 19:26:03 +0000

    finished remove-nth-node of a list

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/removeNodeEndOfList.java
@@ -0,0 +1,32 @@
+package ca.jrvs.practice.codingChallenge;
+
+public class removeNodeEndOfLiist
+{
+  /* Good first attempt will need to
+  *  revise the code and do dry runs
+  * for understanding purposes.
+  * */
+  public ListNode removeNthFromEnd(ListNode head, int n)
+  {
+    ListNode temp = new ListNode(0);
+    ListNode node1 = temp, node2 = temp;
+    node2.next = head;
+
+    for (int i = 0; i < n + 1; i++)
+    {
+      node2 = node2.next;
+    }
+
+    while (node2 != null)
+    {
+      node1 = node1.next;
+      node2 = node2.next;
+    }
+
+    node1.next = node1.next.next;
+    return temp.next;
+
+
+  }
+
+}
