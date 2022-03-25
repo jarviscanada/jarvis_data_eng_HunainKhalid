be901de0432feb17e2e68c25aba847f1b614b0e0
Hunain Khalid <hunainkhalid101@gmail.com>
2022-03-25 15:42:14 +0000

    typo in class name, re-commit

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/removeNodeEndOfList.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/removeNodeEndOfList.java
@@ -1,32 +1,33 @@
 package ca.jrvs.practice.codingChallenge;
 
-public class removeNodeEndOfLiist
-{
-  /* Good first attempt will need to
-   *  revise the code and do dry runs
-   * for understanding purposes.
-   * */
-  public ListNode removeNthFromEnd(ListNode head, int n)
+  public class removeNodeEndOfList
   {
-    ListNode temp = new ListNode(0);
-    ListNode node1 = temp, node2 = temp;
-    node2.next = head;
-
-    for (int i = 0; i < n + 1; i++)
+    /* Good first attempt will need to
+     *  revise the code and do dry runs
+     * for understanding purposes.
+     * */
+    public ListNode removeNthFromEnd(ListNode head, int n)
     {
-      node2 = node2.next;
+      ListNode temp = new ListNode(0);
+      ListNode node1 = temp, node2 = temp;
+      node2.next = head;
+
+      for (int i = 0; i < n + 1; i++)
+      {
+        node2 = node2.next;
+      }
+
+      while (node2 != null)
+      {
+        node1 = node1.next;
+        node2 = node2.next;
+      }
+
+      node1.next = node1.next.next;
+      return temp.next;
+
+
     }
-
-    while (node2 != null)
-    {
-      node1 = node1.next;
-      node2 = node2.next;
-    }
-
-    node1.next = node1.next.next;
-    return temp.next;
-
 
   }
 
-}
