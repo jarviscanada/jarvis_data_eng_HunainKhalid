fbbb2e2b84208d95eed482722411d904ae4c3733
Hunain Khalid <hunainkhalid101@gmail.com>
2022-03-28 18:17:47 +0000

    finished minmaxArray, min portion all three waya

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/minmaxArray.java
@@ -0,0 +1,51 @@
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class minmaxArray
+{
+  // most basic way
+  public static void min0(int[] arr)
+  {
+    int min = Integer.MAX_VALUE;
+
+    for(int i = 0; i < arr.length; i++)
+    {
+      if (min > arr[i])
+      {
+        min = arr[i];
+      }
+    }
+
+    System.out.println(min);
+  }
+
+  // using streams
+  public static void min1(int[] arr)
+  {
+    int min = Arrays
+        .stream(arr)
+        .min()
+        .orElseThrow(NoSuchElementException::new);
+
+    System.out.println(min);
+  }
+
+  // using collections
+  public static void min2(int[] arr)
+  {
+    Arrays.sort(arr);
+    System.out.println(arr[0]);
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {9, 10, 17, 33, 7, 5, 6, 11, 88, 66, 1};
+    min0(arr);
+    min1(arr);
+    min2(arr);
+  }
+}
