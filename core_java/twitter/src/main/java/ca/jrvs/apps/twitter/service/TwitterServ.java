e83634bed98a40fedad76e5cb4c9b9d5053a88cc
Hunain Khalid <hunainkhalid101@gmail.com>
2022-04-25 16:16:20 +0000

    framework for twitterService added

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/TwitterServ.java
@@ -0,0 +1,23 @@
+package ca.jrvs.apps.twitter.service;
+
+import ca.jrvs.apps.twitter.model.Tweet;
+import java.util.List;
+
+public class TwitterServ implements Service
+{
+
+  @Override
+  public Tweet postTweet(Tweet tweet) {
+    return null;
+  }
+
+  @Override
+  public Tweet showTweet(String id, String[] fields) {
+    return null;
+  }
+
+  @Override
+  public List<Tweet> deleteTweets(String[] ids) {
+    return null;
+  }
+}
