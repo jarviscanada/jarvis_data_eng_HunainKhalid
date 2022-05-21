ecfd11d153537ddfc8d9db7e1038448228145457
Hunain Khalid <hunainkhalid101@gmail.com>
2022-05-21 12:19:58 -0400

    added CLI app, will do springboot next

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterCliApp.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterCliApp.java
@@ -6,8 +6,13 @@ import ca.jrvs.apps.twitter.dao.CrdDao;
 import ca.jrvs.apps.twitter.dao.TwitterDao;
 import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
 import ca.jrvs.apps.twitter.dao.helper.TwitterHttpHelper;
+import ca.jrvs.apps.twitter.model.Tweet;
 import ca.jrvs.apps.twitter.service.Service;
 import ca.jrvs.apps.twitter.service.TwitterServ;
+import ca.jrvs.apps.twitter.utilities.JsonUtil;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TwitterCliApp
@@ -43,10 +48,40 @@ public class TwitterCliApp
 
     app.run(args);
   }
-
-  public void run()
+  public void run(String[] args)
   {
+    if (args.length == 0)
+    {
+      throw new IllegalArgumentException("USAGE:");
+    }
+
+    if (args[0].toLowerCase().equals("post"))
+    {
+      controller.postTweet(args);
+    }
+    else if (args[0].toLowerCase().equals("show"))
+    {
+      controller.showTweet(args);
+    }
+    else if (args[0].toLowerCase().equals("delete"))
+    {
+      controller.deleteTweet(args).forEach(this::pTweet);
+    }
 
   }
+
+  private void pTweet(Tweet t)
+  {
+    try
+    {
+      System.out.println(JsonUtil.toJson(t, true, false));
+    }
+    catch (JsonProcessingException e)
+    {
+      throw new RuntimeException("Cannot convert object");
+    }
+  }
+
+
 
 }
